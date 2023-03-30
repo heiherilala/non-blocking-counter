fe28c7aef1eb2e25753038a90dd5307b69819a5e
heiherilala <hei.herilala@gmail.com>
2023-03-30 09:09:25 +0300

    chore(STD21108: Counter)

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
 const Counter = ({from, to}) => {
   const consecutiveNumbers = [...Array(to-from).keys()].map(i => i + from);
-  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>
+  return <div>{consecutiveNumbers.map(n => <p key={n}>{n}</p>)}</div>;
 }
 
 export default Counter;
